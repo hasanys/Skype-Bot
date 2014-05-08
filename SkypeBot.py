2e034364d5a6a99b3cfe615c16183a51e50c1c26
eskeete <eskeete@evertz.com>
2014-05-08 13:33:21 -0400

    Added more 8ball responses

--- SkypeBot.py
+++ SkypeBot.py
@@ -19,7 +19,34 @@ class SkypeBot:
     which_bot = 0
     edit_mode_enabled = False
     bots = ['Chomsky', 'Lauren', 'iEinstein', 'ALICE', 'Cleverbot']
-    magic_ball_responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "All signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+    magic_ball_responses = [
+        "It is certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes definitely",
+        "You may rely on it",
+        "As I see it, yes",
+        "Most likely",
+        "Outlook good",
+         "Yes",
+         "All signs point to yes",
+         "Reply hazy, try again",
+         "Ask again later",
+         "Better not tell you now",
+         "Cannot predict now",
+         "Concentrate and ask again",
+         "Don't count on it",
+         "My reply is no",
+         "My sources say no",
+         "Outlook not so good",
+         "Very doubtful",
+         "Your mom told me so last night",
+         "I don't answer retarded questions",
+         "Go ask a shrink",
+         "Yeh",
+         "Ye",
+         "Idk bruh"
+    ]
     
     def __init__(self):
         self.skype = Skype4Py.Skype()
